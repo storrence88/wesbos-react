a4023d508a79c05c068ad592ea7222512a1fdacc
Steven Torrence <storrence88@gmail.com>
2020-10-04 09:33:20 -0500

    Add stylesheet

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const StorePicker = () => {
   return (
-    <form>
+    <form className='store-selector'>
       <h2>Please Enter a Store</h2>
       <input type='text' required placeholder='Store Name' />
       <button type='submit'>Visit Store</button>
